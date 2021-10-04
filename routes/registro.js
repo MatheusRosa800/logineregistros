37d0cccefe6cb3c0f7a8743fa927e8f3279d1d7d
MatheusRosa800 <61835871+MatheusRosa800@users.noreply.github.com>
2021-10-04 13:00:48 -0300

    Add files via upload

--- routes/registro.js
+++ routes/registro.js
@@ -2,4 +2,24 @@ module.exports = (app)=>{
     app.get('/registro',(req,res)=>{
         res.render('registro.ejs')
     })
+
+app.post('/registro', async(req,res)=>{
+    
+    var conexao = require('../config/database')()
+    var usuarios = require('../models/usuarios')
+
+     var userexiste = await usuarios.findOne({email:req.body.email})
+    if(userexiste){
+        return res.send('email ja cadastrado')
+    }else{
+        var documentos = new usuarios({
+            Nome:req.body.nome,
+            email:req.body.email,
+            senha:req.body.senha
+
+        }).save()
+        res.render('login.ejs')
+        }
+    })
+
 }
