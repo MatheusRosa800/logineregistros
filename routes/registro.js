97c9cd299e9787c879a9cf00e701825e37b02d64
MatheusRosa800 <61835871+MatheusRosa800@users.noreply.github.com>
2021-10-07 12:08:42 -0300

    Add files via upload

--- routes/registro.js
+++ routes/registro.js
@@ -1,29 +1,26 @@
-module.exports = (app)=>{
-    //importar o bcrypt
+module.exports =(app)=>{
+ 
     var bcrypt = require('bcrypt')
-
-    app.get('/registro',(req,res)=>{
+ 
+    app.get('/registro', (req, res)=>{
         res.render('registro.ejs')
     })
-
-app.post('/registro', async(req,res)=>{
-    
-    var conexao = require('../config/database')()
-    var usuarios = require('../models/usuarios')
-
-     var userexiste = await usuarios.findOne({email:req.body.email})
-    if(userexiste){
-        return res.send('email ja cadastrado')
-    }else{
-        var senhasegura = await bcrypt.hash(req.body.senha,12)
-        var documentos = new usuarios({
-            nome:req.body.nome,
-            email:req.body.email,
-            senha:senhasegura
-
-        }).save()
-        res.render('login.ejs')
+ 
+    app.post('/registro', async(req, res)=>{
+        var conexao = require('../config/database')()
+        var usuarios = require('../models/usuarios')
+ 
+        var userexiste = await usuarios.findOne({email:req.body.email})
+        if(userexiste){
+            return res.render('registro.ejs',{mensagem:'Email já cadastrado'})
+        }else{
+          var senhasegura = await bcrypt.hash(req.body.senha,12)
+          var documento = new usuarios({
+                nome:req.body.nome,
+                email:req.body.email,
+                senha:senhasegura
+          }).save()
+          res.render('login.ejs')  
         }
     })
-
 }
