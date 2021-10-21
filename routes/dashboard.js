60e54ada17fe69f604d5fcaec01b228d776fa73b
MatheusRosa800 <61835871+MatheusRosa800@users.noreply.github.com>
2021-10-21 12:02:49 -0300

    Update dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,12 +1,21 @@
 module.exports = (app)=>{
-
-    app.get('/dashboard', (req,res)=>{
+    
+    var conexao = require("../config/database.js")()
+    var usuarios = require("../models/usuarios")
+    
+    app.get('/dashboard',(req,res)=>{
         var id = req.query.id
         console.log|(id)
         if(!id){
             res.redirect('/login')
+        }else{
+            var userexiste = await usuarios.finOne({_id:id})
+            if(userexiste){
+                res.render('dashboard.ejs', {login:userexiste})
+            }else{
+                res.redirect('/login')
+            }    
         }
-        res.render('dashboard.ejs', {login:{nome:'teste', _id:'teste'}})
-    })
+     })
 
 }
