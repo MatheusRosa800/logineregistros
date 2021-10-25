fc6f99e1a222318804144cab96b594121d5cca64
MatheusRosa800 <61835871+MatheusRosa800@users.noreply.github.com>
2021-10-25 11:43:56 -0300

    Add files via upload

---
+++ models/atividades.js
@@ -0,0 +1,12 @@
+var mongoose = require ('mongoose')
+
+var modelo = new mongoose.Schema({
+    data:Date,
+    tipo:String,
+    entrega:String,
+    disciplina:String,
+    instrucoes:String,
+    user:String
+})
+
+module.exports = mongoose.model('atividades', modelo)
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,6 +2,7 @@ module.exports = (app)=>{
     
     var conexao = require("../config/database.js")()
     var usuarios = require("../models/usuarios")
+    var atividades = require("../models/atividades")
     
     app.get('/dashboard',async(req,res)=>{
         var id = req.query.id
@@ -18,4 +19,21 @@ module.exports = (app)=>{
         }
      })
 
+     app.post('/dashboard',async(req,res)=>{
+        var dados = req.body
+
+        var novo = await new atividades ({
+        data: dados.data,
+        tipo: dados.tipo,
+        entrega: dados.entrega,
+        disciplina: dados.disciplina,
+        instrucoes: dados.instrucoe,
+        user: dados.redirect
+    
+     }).save()
+     .then((result)=>{
+         res.redirect('/dashboard?id=' +dados.id)
+     })
+
+    })
 }
