af8d3f7062fd9192b3358d71363885107fcd2ec8
MatheusRosa800 <61835871+MatheusRosa800@users.noreply.github.com>
2021-10-25 13:02:47 -0300

    Add files via upload

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,39 +1,42 @@
 module.exports = (app)=>{
-    
+ 
     var conexao = require("../config/database.js")()
     var usuarios = require("../models/usuarios")
     var atividades = require("../models/atividades")
-    
+ 
     app.get('/dashboard',async(req,res)=>{
         var id = req.query.id
-        console.log|(id)
+        console.log(id)
         if(!id){
             res.redirect('/login')
         }else{
-            var userexiste = await usuarios.finOne({_id:id})
-            if(userexiste){
-                res.render('dashboard.ejs', {login:userexiste})
-            }else{
-                res.redirect("/login")
-            }    
+         var userexiste = await usuarios.findOne({_id:id})
+         if(userexiste){
+            var lista = await atividades.find({user:id})
+            if(lista){
+            res.render('dashboard.ejs',{login:userexiste, lista})
+            }
+            
+         }else{
+            res.redirect('/login')
         }
-     })
-
-     app.post('/dashboard',async(req,res)=>{
-        var dados = req.body
-
-        var novo = await new atividades ({
-        data: dados.data,
-        tipo: dados.tipo,
-        entrega: dados.entrega,
-        disciplina: dados.disciplina,
-        instrucoes: dados.instrucoe,
-        user: dados.redirect
+        }
+    })
     
-     }).save()
-     .then((result)=>{
-         res.redirect('/dashboard?id=' +dados.id)
-     })
-
+    app.post('/dashboard',async(req,res)=>{
+       var dados = req.body
+       
+       var novo = await new atividades({
+           data: dados.data,
+           tipo: dados.tipo,
+           entrega: dados.entrega,
+           disciplina: dados.disciplina,
+           instrucoes: dados.instrucoes,
+           user: dados.id
+        
+       }).save()
+       .than((result)=>{
+           res.redirect('/dashboard?id='+dados.id)
+       })
     })
 }
